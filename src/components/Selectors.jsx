4c54fd7239b7656fe4de242e5d34d143261b8f31
Andrey Torkhov <torkhovasic@yandex.ru>
2024-05-12 22:10:21 +0300

    fix selectors components

--- src/components/Selectors.jsx
+++ src/components/Selectors.jsx
@@ -11,14 +11,23 @@ function Selectors({ updateCatalog }) {
   const [cuisines, setCuisines] = useState([]);
   const [mealTypes, setMealTypes] = useState([]);
 
+  const [allRecipes, setAllRecipes] = useState();
   const [filterRecipes, setFilterRecipes] = useState();
+
+  const [selectedCuisine, setSelectedCuisine] = useState();
+  const [selectedMealType, setSelectedMealType] = useState();
+  const [selectedDifficulty, setSelectedDifficulty] = useState();
 
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       const recipesData = await getAllRecipes();
+      setAllRecipes(recipesData);
       setFilterRecipes(recipesData);
+      setSelectedCuisine(recipesData);
+      setSelectedMealType(recipesData);
+      setSelectedDifficulty(recipesData);
     };
 
     fetchData();
@@ -51,9 +60,8 @@ function Selectors({ updateCatalog }) {
     difficultySelectorRef.current.querySelector(
       'input[type="radio"][value="Любая"]'
     ).checked = true;
-    let allData = await getAllRecipes();
-    setFilterRecipes(allData);
-    updateCatalog(allData);
+    setFilterRecipes(allRecipes);
+    updateCatalog(allRecipes);
     setTimeout(() => {
       setIsLoading(false);
     }, 500);
@@ -121,6 +129,7 @@ function Selectors({ updateCatalog }) {
 
     for (const recipe of recipes.recipes) {
       const recipeDiff = recipe.difficulty;
+      // eslint-disable-next-line
       if (recipeDiff && recipeDiff == difficulity) {
         filteredRecipes.recipes.push(recipe);
       }
@@ -135,8 +144,8 @@ function Selectors({ updateCatalog }) {
     setIsLoading(true);
     const cousine = kitchenSelectorRef.current.value;
     const recipesByCousine = filterRecipesByCuisine(filterRecipes, cousine);
-    setFilterRecipes(recipesByCousine);
-    updateCatalog(recipesByCousine);
+    setSelectedCuisine(recipesByCousine);
+    fetchAndFilter();
     setTimeout(() => {
       setIsLoading(false);
     }, 500);
@@ -146,8 +155,8 @@ function Selectors({ updateCatalog }) {
     setIsLoading(true);
     const mealType = dishTypeSelectorRef.current.value;
     const recipesByMealType = filterRecipesByMealType(filterRecipes, mealType);
-    setFilterRecipes(recipesByMealType);
-    updateCatalog(recipesByMealType);
+    setSelectedMealType(recipesByMealType);
+    fetchAndFilter();
     setTimeout(() => {
       setIsLoading(false);
     }, 500);
@@ -155,16 +164,71 @@ function Selectors({ updateCatalog }) {
 
   const difficultyFilter = (difficulty) => {
     setIsLoading(true);
-    const recipesByDifficulty = filterRecipesByDifficulty(
-      filterRecipes,
-      difficulty
-    );
-    setFilterRecipes(recipesByDifficulty);
-    updateCatalog(recipesByDifficulty);
+    if (difficulty === "Любая") {
+      setSelectedDifficulty(allRecipes);
+    } else {
+      const recipesByDifficulty = filterRecipesByDifficulty(
+        filterRecipes,
+        difficulty
+      );
+      setSelectedDifficulty(recipesByDifficulty);
+    }
+    fetchAndFilter();
     setTimeout(() => {
       setIsLoading(false);
     }, 500);
   };
+
+  async function getAllIds(obj) {
+    const allIds = obj.recipes.map((recipe) => recipe.id).flat();
+    return await allIds;
+  }
+
+  async function findCommonIds(obj1, obj2, obj3) {
+    const ids1 = await getAllIds(obj1);
+    const ids2 = await getAllIds(obj2);
+    const ids3 = await getAllIds(obj3);
+    const commonIds = ids1.filter(
+      (id) => ids2.includes(id) && ids3.includes(id)
+    );
+
+    return await commonIds;
+  }
+
+  async function filterObjectsById(obj, commonIds) {
+    const filteredRecipes = obj.recipes.filter((recipe) =>
+      commonIds.includes(recipe.id)
+    );
+    const filteredObject = {
+      recipes: filteredRecipes,
+      total: filteredRecipes.length,
+      skip: 0,
+      limit: filteredRecipes.length,
+    };
+    return filteredObject;
+  }
+
+  async function fetchAndFilter() {
+    try {
+      if (!selectedCuisine || !selectedMealType || !selectedDifficulty) {
+        return;
+      }
+      const commonIds = await findCommonIds(
+        selectedCuisine,
+        selectedMealType,
+        selectedDifficulty
+      );
+      const filterObj = await filterObjectsById(allRecipes, commonIds);
+      updateCatalog(filterObj);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchAndFilter();
+    // eslint-disable-next-line
+  }, [selectedCuisine, selectedMealType, selectedDifficulty]);
 
   return (
     <div className="selectors-container">
@@ -215,6 +279,7 @@ function Selectors({ updateCatalog }) {
               name="difficulty"
               value="Любая"
               defaultChecked
+              onChange={() => difficultyFilter("Любая")}
             />
             <label htmlFor="any">Любая</label>
           </div>
